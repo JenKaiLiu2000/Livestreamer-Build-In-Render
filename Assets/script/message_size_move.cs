65293f8c55db7eb49c6ef105293816d89b39edd4
JenKaiLiu <jenkailiu0710@gmail.com>
2021-05-25 19:28:33 +0800

    text sending effect. Using coroutine to delay the process.

--- Assets/script/message_size_move.cs
+++ Assets/script/message_size_move.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Text;
 
 public class message_size_move : MonoBehaviour
 {
     [Header("Reference")]
     public GameObject image_prefab;
+    public GameObject text_prefab;
     public GameObject container;
     [Header("Controler")]
     public int listMaxCount = 5;
+    public float slideSpeed = 2;
 
     Vector3 originPos;
     List<GameObject> contentsList = new List<GameObject>();
@@ -25,7 +28,7 @@ public class message_size_move : MonoBehaviour
         //按下空白鍵，產出內容，並將內容丟置聊天室。
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            addContent(createImage());
+            StartCoroutine(waitFrame_addContent(createText()));
         }
         //聊天室的動態更新。
         containerUpdate();
@@ -39,12 +42,20 @@ public class message_size_move : MonoBehaviour
         product.GetComponent<RectTransform>().sizeDelta = new Vector2(200, Random.Range(50, 200));
         return product;
     }
-
+    //content內容產生。  之後會再增加text的版本，從database抓留言出來。
+    GameObject createText()
+    {
+        //將我們先前準備的圖片prefab載入。
+        GameObject product = text_prefab;
+        int repeatNumber = Random.Range(1,20);
+        text_prefab.GetComponent<Text>().text = new StringBuilder("TEXT".Length * repeatNumber).Insert(0, "TEXT", repeatNumber).ToString();
+        return product;
+    }
     //更新container聊天室的移動效果。
     void containerUpdate()
     {
         if (container.transform.position != originPos)
-            container.transform.position = Vector3.Lerp(container.transform.position, originPos, Time.deltaTime * 5);
+            container.transform.position = Vector3.Lerp(container.transform.position, originPos, Time.deltaTime * slideSpeed);
     }
 
     //加入contet內容鑲嵌至container聊天室中。
@@ -76,9 +87,34 @@ public class message_size_move : MonoBehaviour
     //根據輸入的內容大小調整聊天室的所在高度。
     void adj_Container_Height(GameObject _contentInstance)
     {
+        if(_contentInstance.GetComponent<ContentSizeFitter>())
+            _contentInstance.GetComponent<ContentSizeFitter>().verticalFit = ContentSizeFitter.FitMode.Unconstrained;
         //抓取內容的高度。
         float content_hight = _contentInstance.GetComponent<RectTransform>().rect.height;
         //為了做出訊息由下而上的效果，將聊天室的高度-內容的高度，這樣一來，雖然內容新增，但視覺的高度不變，接著再用位移的方式將內容帶出。
         container.transform.position -= new Vector3(0, content_hight, 0);
+    }
+
+    IEnumerator waitFrame_addContent(GameObject _contentInstance)
+    {
+        //==step one==
+        //實例化物件在場景中，先不指定parent，因為它要先在看不到的地方初始化，不然會讓視覺卡頓。
+        GameObject Instance = Instantiate(_contentInstance);
+        //list追蹤以及刪除物件。
+        listTrack(Instance);
+        //如果內容有Content Size Fitter，就讓他自動調整高度。
+        if (Instance.GetComponent<ContentSizeFitter>())
+            Instance.GetComponent<ContentSizeFitter>().verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+        //等待一個frame，讓他初始化。
+        yield return null;
+
+        //==step two==
+        //將實例化的物件設定parent，就自動交給聊天室的對齊工具調整。
+        Instance.transform.SetParent(container.transform);
+        //如果內容有Content Size Fitter，調整完高度要將constrained關閉，不然讀取不了height。
+        if (Instance.GetComponent<ContentSizeFitter>())
+            Instance.GetComponent<ContentSizeFitter>().verticalFit = ContentSizeFitter.FitMode.Unconstrained;
+        //調整聊天室的所在高度。
+        adj_Container_Height(Instance);
     }
 }
