3b2c9344a3cf695ad6164a455fd2d0ab1119e6be
JenKaiLiu <jenkailiu0710@gmail.com>
2021-05-26 14:14:51 +0800

    Dialog box sending effect,and setting value of the Dialog box.

--- Assets/script/message_size_move.cs
+++ Assets/script/message_size_move.cs
@@ -9,10 +9,26 @@ public class message_size_move : MonoBehaviour
     [Header("Reference")]
     public GameObject image_prefab;
     public GameObject text_prefab;
+    public GameObject DialogBox_prefab;
     public GameObject container;
-    [Header("Controler")]
-    public int listMaxCount = 5;
-    public float slideSpeed = 2;
+    [Header("Process Controler")]
+    [Tooltip("內容數限量誌")]
+    public int listMaxCount = 15; //內容數限量誌
+    [Tooltip("動態滑動效果的速度")]
+    public float slideSpeed = 3.5f; //動態滑動效果的速度
+    [Header("Dialog Box Setting")]
+    [Tooltip("字級大小")]
+    public int _fontSize = 30; //字級大小
+    [Tooltip("對話框寬度")]
+    public float textBox_MaxWidth = 450; //對話框寬度
+    [Tooltip("對話框上下padding距離")]
+    public float DialogBox_Top_Botton_padding = 15; //對話框上下padding距離
+    [Tooltip("對話框左右padding距離")]
+    public float DialogBox_Left_Right_padding = 20; //對話框左右padding距離
+    [Tooltip("文字顏色")]
+    public Color textColor = new Color(255,255,255); //文字顏色
+    [Tooltip("對話框顏色")]
+    public Color DialogBoxColor = new Color(95, 95, 95,150); //對話框顏色
 
     Vector3 originPos;
     List<GameObject> contentsList = new List<GameObject>();
@@ -28,7 +44,7 @@ public class message_size_move : MonoBehaviour
         //按下空白鍵，產出內容，並將內容丟置聊天室。
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            StartCoroutine(waitFrame_addContent(createText()));
+            StartCoroutine(waitFrame_addContent(createDialogBox()));
         }
         //聊天室的動態更新。
         containerUpdate();
@@ -39,17 +55,59 @@ public class message_size_move : MonoBehaviour
         //將我們先前準備的圖片prefab載入。
         GameObject product = image_prefab;
         //隨機設定圖片高度(製造出變化性)。
-        product.GetComponent<RectTransform>().sizeDelta = new Vector2(200, Random.Range(50, 200));
+        product.GetComponent<RectTransform>().sizeDelta = new Vector2(textBox_MaxWidth, Random.Range(50, 200));
         return product;
     }
     //content內容產生。  之後會再增加text的版本，從database抓留言出來。
     GameObject createText()
     {
+        //==step one==(產生物件，與初始化)
         //將我們先前準備的圖片prefab載入。
         GameObject product = text_prefab;
-        int repeatNumber = Random.Range(1,20);
-        text_prefab.GetComponent<Text>().text = new StringBuilder("TEXT".Length * repeatNumber).Insert(0, "TEXT", repeatNumber).ToString();
+        //先設定文字框的寬度，高度可以不理他，因為我們有裝Content Size Fitter，他會自動調整，因此，隨便輸入一個數值就好，這邊使用0。
+        product.GetComponent<RectTransform>().sizeDelta = new Vector2(textBox_MaxWidth,0);
+
+        //==step two==(設定文字)
+        //先抓取物件上面的Text Component，避免這串重複打，先將他存在一個物件中。
+        Text textComponent = product.GetComponent<Text>();
+        //隨機產生文字，將它賦予到我們的text內容。
+        textComponent.text = generateRandomString();
+        //根據_fontSize屬性設定字級大小。
+        textComponent.fontSize = _fontSize;
+        //根據textColor屬性設定文字顏色。
+        textComponent.color = textColor;
         return product;
+    }
+    GameObject createDialogBox()
+    {
+        //==step one==(產生物件，與初始化)
+        //將我們先前準備的圖片prefab載入。
+        GameObject product = DialogBox_prefab;
+        //找到帶有Text Component的子物件，接著設定文字框的寬度，高度可以不理他。
+        //因為我們有裝Content Size Fitter，他會自動調整，因此，隨便輸入一個數值就好，這邊使用0。
+        product.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(textBox_MaxWidth, 0);
+
+        //==step two==(設定文字與底圖)
+        //先抓取物件上面的Image Component，避免這串重複打，先將他存在一個物件中。
+        Image imageComponent = product.GetComponent<Image>();
+        //先抓取物件上面的Text Component，避免這串重複打，先將他存在一個物件中。
+        Text textComponent = product.transform.GetChild(0).GetComponent<Text>();
+        //設定對話框的顏色。
+        imageComponent.color = DialogBoxColor;
+        //隨機產生文字，將它賦予到我們的text內容。
+        textComponent.text = generateRandomString();
+        //根據_fontSize屬性設定字級大小。
+        textComponent.fontSize = _fontSize;
+        //根據textColor屬性設定文字顏色。
+        textComponent.color = textColor;
+        return product;
+    }
+    string generateRandomString()
+    {
+        //隨機產生數字，是我們等等字串repeat的次數。
+        int repeatNumber = Random.Range(1, 20);
+        //StringBuilder可以將字串重複。
+        return new StringBuilder("Text".Length * repeatNumber).Insert(0, "Text", repeatNumber).ToString(); ;
     }
     //更新container聊天室的移動效果。
     void containerUpdate()
@@ -87,14 +145,22 @@ public class message_size_move : MonoBehaviour
     //根據輸入的內容大小調整聊天室的所在高度。
     void adj_Container_Height(GameObject _contentInstance)
     {
-        if(_contentInstance.GetComponent<ContentSizeFitter>())
-            _contentInstance.GetComponent<ContentSizeFitter>().verticalFit = ContentSizeFitter.FitMode.Unconstrained;
         //抓取內容的高度。
         float content_hight = _contentInstance.GetComponent<RectTransform>().rect.height;
         //為了做出訊息由下而上的效果，將聊天室的高度-內容的高度，這樣一來，雖然內容新增，但視覺的高度不變，接著再用位移的方式將內容帶出。
         container.transform.position -= new Vector3(0, content_hight, 0);
     }
-
+    //根據設定的屬性，調整對話框的padding。
+    GameObject adj_Content_padding(GameObject _contentInstance)
+    {
+        //抓取目前對話框的寬高。
+        float w = _contentInstance.GetComponent<RectTransform>().rect.width;
+        float h = _contentInstance.GetComponent<RectTransform>().rect.height;
+        //再將設定的padding加上去。
+        _contentInstance.GetComponent<RectTransform>().sizeDelta = new Vector2(w + DialogBox_Left_Right_padding, h + DialogBox_Top_Botton_padding);
+        return _contentInstance;
+    }
+    //延遲1 frame執行，並且開關Content Size Fitter的設定。
     IEnumerator waitFrame_addContent(GameObject _contentInstance)
     {
         //==step one==
@@ -104,7 +170,10 @@ public class message_size_move : MonoBehaviour
         listTrack(Instance);
         //如果內容有Content Size Fitter，就讓他自動調整高度。
         if (Instance.GetComponent<ContentSizeFitter>())
+        {
             Instance.GetComponent<ContentSizeFitter>().verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+            Instance.GetComponent<ContentSizeFitter>().horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+        }
         //等待一個frame，讓他初始化。
         yield return null;
 
@@ -113,7 +182,12 @@ public class message_size_move : MonoBehaviour
         Instance.transform.SetParent(container.transform);
         //如果內容有Content Size Fitter，調整完高度要將constrained關閉，不然讀取不了height。
         if (Instance.GetComponent<ContentSizeFitter>())
+        {
             Instance.GetComponent<ContentSizeFitter>().verticalFit = ContentSizeFitter.FitMode.Unconstrained;
+            Instance.GetComponent<ContentSizeFitter>().horizontalFit = ContentSizeFitter.FitMode.Unconstrained;
+        }
+        //調整對話框的padding。
+        adj_Content_padding(Instance);
         //調整聊天室的所在高度。
         adj_Container_Height(Instance);
     }
